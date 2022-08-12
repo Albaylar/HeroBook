d9f98a0cfed11e541c6bd5d68f756dd42a1bedce
Albaylar <furkandenizalbaylar@gmail.com>
2022-08-12 13:54:31 +0300

    Initial Commit

---
+++ HeroBook/HeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroBookApp.swift
+//  HeroBook
+//
+//  Created by Furkan Deniz Albaylar on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
